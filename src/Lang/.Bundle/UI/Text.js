be65a0a6975f3b0457a8839e58c812fc8b3ee1c8
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-11-28 13:28:11 +0300

    -

--- src/Lang/.Bundle/UI/Text.js
+++ src/Lang/.Bundle/UI/Text.js
@@ -35,6 +35,10 @@ App.Modules.Lang.UI.Text = class extends Colibri.UI.Forms.Object {
         });
     }
 
+    get value() {
+        return super.value;
+    }
+
 }
 
 Colibri.UI.Forms.Field.RegisterFieldComponent('Lang.UI.Text', 'App.Modules.Lang.UI.Text', '#{lang-fields-text;Текст (мультиязычный)}', App.Modules.Lang.Icons.Text);
