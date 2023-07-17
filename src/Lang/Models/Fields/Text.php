9db2e9d2245e7895c6d51cadb7bb9b4076c2a1f9
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-07-17 10:12:54 +0300

    -

--- src/Lang/Models/Fields/Text.php
+++ src/Lang/Models/Fields/Text.php
@@ -47,4 +47,10 @@ class Text extends ObjectField
 
     }
 
+    public function Translate(): string
+    {
+        $langModule = App::$moduleManager->Get('lang');
+        return $langModule->Translate($this->ToArray(true));
+    }
+
 }
