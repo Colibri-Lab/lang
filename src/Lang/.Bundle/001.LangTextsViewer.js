31c1b1ab52f556d61b17b84507d0cbe435557dc7
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2024-07-01 10:13:13 +0300

    -

--- src/Lang/.Bundle/001.LangTextsViewer.js
+++ src/Lang/.Bundle/001.LangTextsViewer.js
@@ -15,6 +15,14 @@ App.Modules.Lang.LangTextsViewer = class extends Colibri.UI.Viewer {
     set value(value) {
         
         this._value = value;
+
+        const vv = [];
+        Object.forEach(value, (k, v) => {
+            if(v) {
+                vv.push(k + ': ' + v);
+            }
+        });
+        super.value = vv.join('<br />');
         
         // super.value = this._value && this._value.toShortRUString();
 
